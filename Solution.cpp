9a3b3a4dfd5b5692ec3ec1c30b49e78268470b33
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-10-31 05:06:04 +0100

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,24 @@
+
+#include <vector>
+using namespace std;
+
+class Solution {
+    
+public:
+    bool checkSubarraySum(const vector<int>& input, int target) const {
+        unordered_set<int> previousPrefixSumModuloTarget;
+        int previousSum = 0;
+        int currentSum = 0;
+
+        for (const auto& value : input) {
+            currentSum += value;
+            //C++20: ...contains(currentSum % target), done the old way for compatibility.
+            if (previousPrefixSumModuloTarget.find(currentSum % target) != previousPrefixSumModuloTarget.end()) {
+                return true;
+            }
+            previousPrefixSumModuloTarget.insert(previousSum % target);
+            previousSum = currentSum;
+        }
+        return false;
+    }
+};
